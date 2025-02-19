31f07f468b248815c2e7d9a38728cc072890403d
wolflieu201105 <201105phamgiahuy@gmail.com>
2025-02-19 17:03:45 -0500

    printBoard function

--- worstBot.py
+++ worstBot.py
@@ -30,56 +30,50 @@ def indexToMove(row, col):
     else:
         return chr(100 + abs(3 - row)) + str(7 - row)
 
+def printBoard(board):
+    for i in range(7):
+        if (abs(3 - i) == 3):
+            print("\t" + str(board[i][0]) + "\t" + "\t" + "\t" + str(board[i][1]) + "\t" + "\t" + "\t" + str(board[i][2]))
+        elif (abs(3 - i) == 2):
+            print("\t" + "\t" + str(board[i][0]) + "\t" + "\t" + str(board[i][1]) + "\t" + "\t" + str(board[i][2]) + "\t")
+        elif (abs(3 - i) == 1):
+            print("\t" + "\t" + "\t" + str(board[i][0]) + "\t" + str(board[i][1]) + "\t" + str(board[i][2]) + "\t" + "\t")
+        else:
+            print("\t" + str(board[i][0]) + "\t" +  str(board[i][1]) + "\t" +  str(board[i][2]) + "\t" + "\t" +  str(board[i][3]) + "\t" +  str(board[i][4]) + "\t" +  str(board[i][5]))
+
 def main():
-    print(moveToIndex("a1"))
-    print(moveToIndex("b2"))
-    print(moveToIndex("c3"))
-    print(moveToIndex("d5"))
-    print(moveToIndex("e4"))
-    print(moveToIndex("f6"))
-    print(moveToIndex("g7"))
-    print(moveToIndex("a4"))
-
-    print(indexToMove(6,0))
-    print(indexToMove(5,0))
-    print(indexToMove(4,0))
-    print(indexToMove(2,1))
-    print(indexToMove(3,3))
-    print(indexToMove(1,2))
-    print(indexToMove(0,2))
-    print(indexToMove(3,0))
-    return
-    # board = [[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]]
-    # blue = True
-    # myTurn = False
-    # turns = 0
-    # game_input = input().strip()
-    # if game_input == "blue":
-    #     myTurn = True
-    # elif game_input == "orange":
-    #     myTurn = False
-    #     blue = False
-    # else:
-    #     print("Invalid input")
-    #     sys.exit(1)
-    # while True:
-    #     try:
-    #         if (myTurn):
-    #             if (blue):
-    #                 print("h1 a4 r0", flush=True)
-    #             else:
-    #                 print("h2 b4 r0", flush=True)
-    #             myTurn = False
-    #             turns += 1
+    board = [[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]]
+    printBoard(board)
+    blue = True
+    myTurn = False
+    turns = 0
+    game_input = input().strip()
+    if game_input == "blue":
+        myTurn = True
+    elif game_input == "orange":
+        myTurn = False
+        blue = False
+    else:
+        print("Invalid input")
+        sys.exit(1)
+    while True:
+        try:
+            if (myTurn):
+                if (blue):
+                    print("h1 a4 r0", flush=True)
+                else:
+                    print("h2 b4 r0", flush=True)
+                myTurn = False
+                turns += 1
             
-    #         else:
-    #             move = input().strip()
-    #             # modify the board
-    #             myTurn = True
-    #             turns += 1
+            else:
+                move = input().strip()
+                # modify the board
+                myTurn = True
+                turns += 1
 
-    #     except EOFError:
-    #         break
+        except EOFError:
+            break
 
 if __name__ == "__main__":
     main()
